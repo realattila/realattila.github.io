67ca299304ba64f77382ef89ad9e6b0afc947da2
[ATTILA] <98atila@gmail.com>
2024-05-03 16:34:01 +0330

    🛠️ fix: meta data in main layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,8 +6,22 @@ import { clsx } from "clsx";
 import { APP_KEYS } from "@/utils/keys";
 import { serverTranslation } from "@/i18n";
 import { cookies } from "next/headers";
+import { Metadata } from "next";
 
 const inter = Inter({ subsets: ["latin"] });
+
+export async function generateMetadata({}): Promise<Metadata> {
+  const { t } = await serverTranslation("en", "main", {
+    keyPrefix: "mainLayout.metadata",
+  });
+
+  const domain = process.env.NEXT_PUBLIC_DOMAIN;
+
+  return {
+    title: t("title"),
+    description: t("description"),
+  };
+}
 
 export default async function RootLayout({
   children,
@@ -23,8 +37,6 @@ export default async function RootLayout({
   return (
     <html lang='en' dir='ltr' className={clsx([{ ["tw-dark"]: cookieTheme === "dark" }])}>
       <head>
-        <title>{t("title")}</title>
-        <meta content={t("description")} name='description' />
         <link
           rel='stylesheet'
           href='https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.8.0/styles/github-dark.min.css'></link>
